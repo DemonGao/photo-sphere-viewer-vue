8ab895a720e59d3aff1b78a444bd8f104efe32a2
Demon <750229099@qq.com>
2020-09-30 14:26:13 +0800

    update dist

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,12 @@ const webpack = require('webpack')
 module.exports = {
   publicPath: process.env.BASE_URL + '/',
   chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].title= 'photo-sphere-viewer vue3.0 Demo'
+        return args
+      })
     config.module
       .rule('md')
       .test(/\.md$/)
